4fca57e0ca2a0f05117d74a60aaeec893d7aea58
nothinn <simon.thye@gmail.com>
2019-03-19 17:24:53 +0100

    Changed back testfile from wrong statement

--- j--/tests/Step2/ConditionalExpression.java
+++ j--/tests/Step2/ConditionalExpression.java
@@ -4,7 +4,7 @@ import java.lang.System;
 public class ConditionalExpression {
     public static void main(String[] args) {
         int x = Integer.parseInt(args[0]);
-        System.out.println(x % 2 || 0 ? "even" : "odd");
+        System.out.println(x % 2 == 0 ? "even" : "odd");
         boolean y = true;
         boolean z = false;
         boolean test =false;
