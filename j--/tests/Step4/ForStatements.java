4b0dbbd732f4b3df886840e1f44dbd6a28c85709
nothinn <simon.thye@gmail.com>
2019-05-02 12:14:17 +0200

    Further work on enhanced for loop

--- j--/tests/Step4/ForStatements.java
+++ j--/tests/Step4/ForStatements.java
@@ -7,9 +7,9 @@ import java.lang.System;
 public class ForStatements {
     public static void main(String[] args) {
         int sum1 = 0, sum2 = 0;
-    /*    for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) {
             sum1 += i;
-        }*/
+        }
         int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for (int i : a) {
             sum2+= i;
